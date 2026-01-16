f45cc983d50f20ffbe76bae04de23d35a95616dd
Joe M. <tiredredstoner@gmail.com>
2026-01-15 16:27:31 -0800

    Server code placeholder fix

--- libs/server.js
+++ libs/server.js
@@ -1,6 +1,2 @@
 // Supabase database library
-let server
-(async function () {
-    server = await import("https://unpkg.com/@supabase/supabase-js@2")
-})()
-const serverUrl = "https://gzykzdsyjbxommolnzvx.supabase.co"
+import "https://unpkg.com/@supabase/supabase-js@2"
