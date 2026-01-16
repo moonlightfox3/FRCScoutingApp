423be4eb24d5600f67a792030b4973260efb23b3
Joe M. <tiredredstoner@gmail.com>
2026-01-15 17:36:49 -0800

    Server code progress

--- libs/server.js
+++ libs/server.js
@@ -1,1 +1,30 @@
+// This uses a Supabase database to store data
 
+// Config
+const supabaseServerUrl = "https://gzykzdsyjbxommolnzvx.supabase.co"
+const supabasePublishableApiKey = "sb_publishable_CyMyZ3IKILdgjj96CC-Ozg_PhoPRES6" // You need to sign in to do anything, this is fine
+
+// Client
+let supabaseClient = null
+async function setupClientSupabase () {
+    supabaseClient = supabase.createClient(supabaseServerUrl, supabasePublishableApiKey)
+}
+addEventListener("load", () => setupClientSupabase())
+
+// Authentication
+async function signInToSupabase (email, password) {
+    await supabaseClient.auth.signInWithPassword({email, password})
+}
+async function signOutOfSupabase () {
+    await supabaseClient.auth.signOut()
+}
+
+// Access
+async function getEventListSupabase () {
+    let {data} = await supabaseClient.from("events").select("name,is_current,is_testing")
+    return data
+}
+async function getEventDataSupabase (name) {
+    let {data} = await supabaseClient.from(name).select("file")
+    return data
+}
