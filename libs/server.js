535b9cbc2e30f4705d19b7c4f98a3e496b81fbe8
Joe M. <tiredredstoner@gmail.com>
2026-01-15 19:12:13 -0800

    Server code

--- libs/server.js
+++ libs/server.js
@@ -37,7 +37,7 @@ async function isSignedInToSupabase () {
     }
 }
 
-// Access
+// Access event list
 async function getEventListSupabase () {
     try {
         let {data} = await supabaseClient.from("events").select("name,is_current,is_testing")
@@ -46,11 +46,42 @@ async function getEventListSupabase () {
         return null
     }
 }
-async function getEventDataSupabase (name = "") {
+
+// Access event data
+async function getEventDataSupabase (name) {
     try {
-        let {data} = await supabaseClient.from(name).select("file")
+        let {data} = await supabaseClient.from(name).select("file,file_name")
         return data
     } catch (er) {
         return null
     }
+}
+async function addEventDataSupabase (name, file, fileName) {
+    try {
+        await supabaseClient.from(name).insert([
+            {file, file_name: fileName},
+        ]).select()
+        return true
+    } catch (er) {
+        return false
+    }
+}
+async function editEventDataSupabase (name, fileName, newFileData = null, newFileName = null) {
+    if (newFileData == null && newFileName == null) return false
+
+    let opts = {}
+    if (newFileData != null) opts.file = newFileData
+    if (newFileName != null) opts.file_name = newFileName
+
+    try {
+        await supabaseClient.from(name).update(opts)
+            .eq("file_name", fileName).select()
+        return true
+    } catch (er) {
+        return false
+    }
+}
+async function deleteEventDataSupabase (name, fileName) {
+    await supabaseClient.from(name).delete()
+        .eq("file_name", fileName)
 }
