741c21c482730d172542140ae89c4fa1f0917850
Joe M. <tiredredstoner@gmail.com>
2026-02-17 12:26:01 -0800

    Service worker

--- sw.js
+++ sw.js
@@ -241,6 +241,6 @@ broadcast.onmessage = async function (ev) {
         }
         
         broadcast.postMessage({sender: "sw", type: "github", msg: {commitId, commitDate, deployedToPages, didUpdate: didUpdate || !(await checkCache())}})
-        if (deployedToPages) didUpdate = false
+        didUpdate = false
     }
 }
