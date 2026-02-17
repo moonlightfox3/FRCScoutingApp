7339924effb1e309c72a93ca4ecf5cc5941d8cbc
Joe M. <tiredredstoner@gmail.com>
2026-02-17 14:55:15 -0800

    Logging fix

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Imports
 log("[SW] Script loaded")
-importScripts("/FRCScoutingApp/libs/github.js")
+importScripts("/FRCScoutingApp/libs/debug.js", "/FRCScoutingApp/libs/github.js")
 
 // Caching
 const cachePrefix = "FRCScoutingApp_"
